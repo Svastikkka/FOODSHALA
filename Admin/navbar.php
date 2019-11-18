1eeee406b7de7ef6c4e12cc56bc1faf0d8c7511f
Manshu Sharma <36654754+Svastikkka@users.noreply.github.com>
2019-11-18 16:58:43 +0530

    Update navbar.php

--- Admin/navbar.php
+++ Admin/navbar.php
@@ -1,3 +1,19 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['admin_username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['admin_username']);
+  	header("location: login.php");
+  }
+?>
+
+
+
 <nav class="navbar navbar-default">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -12,3 +28,39 @@
     </div>
   </div><!-- /.container-fluid -->
 </nav>
+
+
+
+<div class="header">
+	<h2>Home Page</h2>
+</div>    
+ <div class="user-content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['admin_username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['admin_username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+
+
+
+
+
+
+
+
+
+
+
